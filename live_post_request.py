9e7c1c33cfb4fc9880cb88d48bf1052054f7ac4d
M78152 <mihaela-daniela.morariu@eon-romania.ro>
2024-03-04 19:30:20 +0200

    add live post request

---
+++ live_post_request.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+data = {
+    "age": 50,
+    "workclass": " Self-emp-not-inc",
+    "fnlgt": 83311,
+    "education": " Bachelors",
+    "education-num": 13,
+    "marital-status": " Married-civ-spouse",
+    "occupation": " Exec-managerial",
+    "relationship": " Husband",
+    "race": " White",
+    "sex": " Male",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 13,
+    "native-country": " United-States",
+}
+response = requests.post(
+    "https://fast-api-project-vzj8.onrender.com//predict/", data=json.dumps(data)
+)
+
+# 3.75.158.163
+# 3.125.183.140
+
+
+print(response.status_code)
+print(response.json())
